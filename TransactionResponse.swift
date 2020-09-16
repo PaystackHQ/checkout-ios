3d49bbfdd01a93487da9bf7b6b9feb5056856d6d
Jubril Olambiwonnu <jubril@paystack.com>
2020-09-16 22:14:53 +0100

    Create TransactionResponse model

---
+++ TransactionResponse.swift
@@ -0,0 +1,12 @@
+//
+//  TransactionResponse.swift
+//  PaystackCheckout
+//
+//  Created by Jubril Olambiwonnu on 9/16/20.
+//
+
+import Foundation
+public struct TransactionResponse {
+    public var id: String
+    public var reference: String
+}

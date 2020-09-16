db519f5d52157d71a7653ca9ad9035a2dd977a13
Jubril Olambiwonnu <jubril@paystack.com>
2020-09-16 22:12:35 +0100

    Create error response model

---
+++ PaystackCheckout/Classes/ErrorResponse.swift
@@ -0,0 +1,24 @@
+//
+//  ErrorResponse.swift
+//  PaystackCheckout
+//
+//  Created by Jubril Olambiwonnu on 9/16/20.
+//
+
+import Foundation
+
+public struct ErrorResponse: Error, CustomStringConvertible {
+    public var description: String {return "APIErrorMessage: \(message)"}
+    let message: String
+    static let genericError = ErrorResponse(message: "Error parsing response")
+    enum CodingKeys: String, CodingKey {
+        case message
+    }
+}
+
+extension ErrorResponse: Decodable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.message = try container.decode(String.self, forKey: .message)
+    }
+}

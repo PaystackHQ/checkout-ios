b2d826ce92a85b71d5154b8978b66afd92241436
Jubril Olambiwonnu <jubril@paystack.com>
2020-09-16 22:05:47 +0100

    Load checkout in a web view an listen for postMessage events

--- CheckoutViewController.swift
+++ CheckoutViewController.swift
@@ -9,12 +9,12 @@ import UIKit
 import WebKit
 
 public class CheckoutViewController: UIViewController {
-
+    
     var webView = WKWebView()
     public var params: PaymentParams
     public var delegate: CheckoutDelegate
     var viewModel = CheckoutViewModel()
-   
+    
     public init(params: PaymentParams, delegate: CheckoutDelegate) {
         self.params = params
         self.delegate = delegate
@@ -28,20 +28,75 @@ public class CheckoutViewController: UIViewController {
     public override func viewDidLoad() {
         super.viewDidLoad()
         addWebView()
-        APIClient.shared.requestInline(params: params)
+        APIClient.shared.requestInline(params: params) { [weak self] response, error in
+            guard let response = response else {
+                // handle error
+                return
+            }
+            print(response.url)
+            let urlRequest = URLRequest(url: URL(string: response.url)!)
+            self?.webView.load(urlRequest)
+        }
     }
     
     func addWebView() {
-
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.configuration.preferences.javaScriptEnabled = true
+        webView.configuration.userContentController.add(self, name: "iosListener")
         view.addSubview(webView)
         webView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         webView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         webView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         webView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
+    
+    func handleResponse(response: Any) {
+        if let bodyDict = response as? [String : Any] {
+            print(bodyDict)
+            guard let event  = bodyDict["event"] as? String else { return }
+            switch event {
+            case TransactionEvents.close:
+                dismiss(animated: true)
+            case TransactionEvents.success:
+                guard let response =  parseTransactionResponse(dict: bodyDict) else {
+                    delegate.onError(error: APIError.emptyResponseError)
+                    return
+                }
+                delegate.onSuccess(response: response)
+            default:
+                break
+            }
+        }
+    }
+    
+    func parseTransactionResponse(dict: [String : Any]) -> TransactionResponse?  {
+        if let data = dict["data"] as? [String : Any] {
+            guard let id = data["trans"] as? String, let reference = data["reference"] as? String else { return nil}
+            return TransactionResponse(id: id, reference: reference)
+        }
+        return nil
+    }
+}
+
+extension CheckoutViewController: WKScriptMessageHandler {
+    public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        print("message received")
+        if message.name == "iosListener" {
+            handleResponse(response: message.body)
+        }
+    }
+    
 }
 
 public protocol CheckoutDelegate {
     func onError(error: Error)
-    func onSuccess(ref: String)
+    func onSuccess(response: TransactionResponse)
+}
+
+struct TransactionEvents {
+    static let close = "close"
+    static let success = "success"
+    static let loadedCheckout = "loadedCheckout"
+    static let loadedTransaction = "loadedTransaction"
+    static let redirecting = "redirecting"
 }

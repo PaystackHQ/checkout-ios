b71985e3ae4ae08392ae56a095aa090b59b6c618
Jubril Olambiwonnu <jubril@paystack.com>
2020-10-07 19:48:25 +0100

    Add error description to ErrorResponse type

--- PaystackCheckout/Classes/ErrorResponse.swift
+++ PaystackCheckout/Classes/ErrorResponse.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public struct ErrorResponse: Error, CustomStringConvertible {
-    public var description: String {return "APIErrorMessage: \(message)"}
+    public var description: String {return message}
     let message: String
     static let genericError = ErrorResponse(message: "Error parsing response")
     enum CodingKeys: String, CodingKey {
@@ -20,5 +20,11 @@ extension ErrorResponse: Decodable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.message = try container.decode(String.self, forKey: .message)
+    }
+}
+
+extension ErrorResponse: LocalizedError {
+    public var errorDescription: String? {
+        return NSLocalizedString(message, comment: "")
     }
 }

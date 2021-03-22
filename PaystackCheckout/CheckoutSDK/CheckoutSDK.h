9faae2e6168ef11f7fc597cb9d9d7d70b7ec76ab
Ravisankar <ravi@paystack.com>
2021-03-22 16:20:04 +0530

    Added an xcode projj for the exising sdk classes

---
+++ PaystackCheckout/CheckoutSDK/CheckoutSDK.h
@@ -0,0 +1,18 @@
+//
+//  CheckoutSDK.h
+//  CheckoutSDK
+//
+//  Created by Ravisankar R on 22/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CheckoutSDK.
+FOUNDATION_EXPORT double CheckoutSDKVersionNumber;
+
+//! Project version string for CheckoutSDK.
+FOUNDATION_EXPORT const unsigned char CheckoutSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheckoutSDK/PublicHeader.h>
+
+

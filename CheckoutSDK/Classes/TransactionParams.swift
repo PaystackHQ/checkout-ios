220e1845dcbfdf59ed7c035419a8ec0ca6ce8507
Jubril Olambiwonnu <57099127+jubril-paystack@users.noreply.github.com>
2021-07-13 11:11:51 +0100

    Added missing params to transaction params class

--- CheckoutSDK/Classes/TransactionParams.swift
+++ CheckoutSDK/Classes/TransactionParams.swift
@@ -14,25 +14,28 @@ public struct TransactionParams {
     public var email: String
     /// Your business' public key found at https://dashboard.paystack.com/keys
     public var publicKey: String
-    /// The customer's first name
     public var firstName: String?
-    /// The customer's last name
     public var lastName: String?
-    /// The customer's phone number
     public var phone: String?
+    public var reference: String?
     public var plan: String?
     public var invoiceLimit: Int?
     public var subAccount: String?
     public var transactionCharge: Int?
     public var bearer: String?
+    public var metaData: String?
+    public var quantity: Int?
+    public var splitCode: String?
+    public var label: String?
     public var currency: Currency?
     public var channels: [Channel]?
     
-    public init(amount: Int, email: String, key: String, currency: Currency? = nil, firstName: String? = nil, lastName: String? = nil, phone: String? = nil, plan: String? = nil, invoiceLimit: Int? = nil, subAccount: String? = nil, transactionCharge: Int? = nil, bearer: String? = nil, channels: [Channel]? = nil) {
+    public init(amount: Int, email: String, key: String, currency: Currency? = nil, firstName: String? = nil, lastName: String? = nil, phone: String? = nil, reference: String? = nil, plan: String? = nil, invoiceLimit: Int? = nil, subAccount: String? = nil, transactionCharge: Int? = nil, bearer: String? = nil, metaData: String? = nil, quantity: Int? = nil, splitCode: String? = nil, label: String? = nil, channels: [Channel]? = nil) {
         self.amount = amount
         self.email = email
         self.publicKey = key
         self.currency = currency
+        self.reference = reference
         self.firstName = firstName
         self.lastName = lastName
         self.phone = phone
@@ -42,6 +45,10 @@ public struct TransactionParams {
         self.transactionCharge = transactionCharge
         self.bearer = bearer
         self.channels = channels
+        self.metaData = metaData
+        self.quantity = quantity
+        self.splitCode = splitCode
+        self.label = label
     }
     
     public enum Currency: String {
@@ -57,5 +64,7 @@ public struct TransactionParams {
         case ussd
         case qr
         case mobileMoney = "mobile_money"
+        case bankTransfer = "bank_transfer"
+        case eft
     }
 }
